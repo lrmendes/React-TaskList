69fec0eb963abad554be231ff56849e2da387218
lrmendes <lrmen14@gmail.com>
2022-05-14 20:39:29 +0100

    Feat(test): testing library configuration (jest, react-testing-library)

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  moduleDirectories: ['node_modules', 'src'],
+  rootDir: 'src',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+}
