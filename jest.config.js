88571e953a1c9585fa770e9e46701b52cd6541e4
lrmendes <lrmen14@gmail.com>
2022-05-15 04:15:26 +0100

    Fix(test): updating jest config to support SVG

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,11 @@ module.exports = {
   rootDir: 'src',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
+    '^.+\\.svg$': '<rootDir>/svgTransform.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
+  testPathIgnorePatterns: ['<rootDir>/svgTransform.js'],
+  coveragePathIgnorePatterns: ['<rootDir>/svgTransform.js'],
 }
---
+++ src/svgTransform.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  process(_, sourcePath) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    }
+  },
+  getCacheKey() {
+    return 'svgTransform'
+  },
+}
