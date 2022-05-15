38e426554aabb783983b49efb5b8cbb62d874c68
Lucas Mendes <lrmen14@gmail.com>
2022-05-16 00:58:46 +0100

    Feat(api): adding random refresh in GH Pages

--- src/api/tasks.tsx
+++ src/api/tasks.tsx
@@ -6,7 +6,8 @@ import mockTaskList from './mocks/taskList.json'
 
 const getTasksAPI = async () => {
   // Applied to GH Deploy only (must be removed in prod)
-  if (window.location.host.includes('.github.io')) return mockTaskList
+  if (window.location.host.includes('.github.io'))
+    return mockTaskList.slice(0, Math.floor(Math.random() * mockTaskList.length))
 
   const response = await fetch(`${BACKEND_API}/tasks`)
   const data: ITask[] = await response.json()
