cec933583ea23ffa9dbf328a8376ad64031476b4
lrmendes <lrmen14@gmail.com>
2022-05-15 01:24:08 +0100

    Feat(UI): adding Drawer component

---
+++ src/routes/routes.tsx
@@ -0,0 +1,8 @@
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import * as React from 'react'
+
+export const routesMenu = [
+  { title: 'Home', path: '/home', icon: <HomeOutlinedIcon /> },
+  { title: 'Tasks', path: '/tasks', icon: <AssignmentOutlinedIcon /> },
+]
